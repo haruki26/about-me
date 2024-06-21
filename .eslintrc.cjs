372ddaa032cd59619abf90e78e113b8077d55922
はるき” <hu@example.com>
2024-06-21 23:53:47 +0900

    initial commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,20 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'standard-with-typescript',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint',
+    'jsx-a11y',
+    'react-hooks',
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
