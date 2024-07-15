45d9a64ed92ae698bd091c9f76faacdbac7e5aa3
はるき” <hu@example.com>
2024-07-15 14:19:54 +0900

    chore: Update Contact component with form validation and error handling

--- src/components/Maincontents/Contact.tsx
+++ src/components/Maincontents/Contact.tsx
@@ -1,28 +1,31 @@
-import { FC, FormEvent, HtmlHTMLAttributes, useEffect, useState } from "react";
+import { ChangeEvent, FC, FormEvent, useState } from "react";
 
 const Contact: FC = () => {
     const [isName, setIsName] = useState<string>("");
     const [isEmail, setIsEmail] = useState<string>("");
     const [isMessage, setIsMessage] = useState<string>("");
     const [isUrlMessage, setIsUrlMessage] = useState<string>("please_fill_in_the_form");
+    const [isUrlColor, setIsUrlColor] = useState<string>("");
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         
-        let formatUrl: string = ""
-
         if (isName && isEmail && isMessage) {
             setIsUrlMessage("success.thank_you_for_contacting_me");
+            setIsUrlColor("text-cyan-400");
 
             setIsName("");
             setIsEmail("");
             setIsMessage("");
         } else {
+            let formatUrl: string = ""
+
             !isName ? formatUrl += "name" : formatUrl += "";
             !isEmail ? formatUrl += (formatUrl && "&") + "email" : formatUrl += "";
             !isMessage ? formatUrl += (formatUrl && "&") + "message" : formatUrl += "";
 
             setIsUrlMessage(`failed.please_fill_in_${formatUrl}`);
+            setIsUrlColor("text-red-500");
         }
     };
 
@@ -31,7 +34,7 @@ const Contact: FC = () => {
         <div className="max-w-screen-sm mx-auto">
             <div className="mx-3 mockup-browser bg-slate-900 border">
                 <div className="mockup-browser-toolbar">
-                    <div className="input">{isUrlMessage}</div>
+                    <div className={`input ${isUrlColor}`}>{isUrlMessage}</div>
                 </div>
                 <div className="bg-slate-800">
                     <form className="flex flex-col gap-4 p-3 font-kosugiMaru" onSubmit={handleSubmit}>
@@ -40,7 +43,7 @@ const Contact: FC = () => {
                             <input
                             type="text"
                             value={isName}
-                            onChange={(e) => setIsName(e.target.value)}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => setIsName(e.target.value)}
                             placeholder="山田太郎"
                             className="p-1 border" />
                         </div>
@@ -49,7 +52,7 @@ const Contact: FC = () => {
                             <input
                             type="email"
                             value={isEmail}
-                            onChange={(e) => setIsEmail(e.target.value)}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => setIsEmail(e.target.value)}
                             placeholder="email@example.com"
                             className="p-1 border" />
                         </div>
@@ -57,7 +60,7 @@ const Contact: FC = () => {
                             <label>お問い合わせ内容</label>
                             <textarea
                             value={isMessage}
-                            onChange={(e) => setIsMessage(e.target.value)}
+                            onChange={(e: ChangeEvent<HTMLTextAreaElement>) => setIsMessage(e.target.value)}
                             placeholder="花冠作成の依頼"
                             className="p-1 border" />
                         </div>
