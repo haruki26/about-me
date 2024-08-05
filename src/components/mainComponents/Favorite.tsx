13c24f3e415653acbe3cbc647be5354c569b8e66
haruki26 <167726701+haruki26@users.noreply.github.com>
2024-08-05 18:47:38 +0900

    refactor: Update Favorite component structure and imports, and fix typos in component imports

--- src/components/mainComponents/Favorite.tsx
+++ src/components/mainComponents/Favorite.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react";
 import FavoritePhoto from "./favorite/FavoritePhoto";
 import Baking from "./favorite/Baking";
+import Foods from "./favorite/Foods";
 
 const Favorite: FC = () => {
     return (
         <div className="p-auto w-full">
-            <div className="flex flex-col justify-center gap-10 md:flex-row md:gap-3">
+            <div className="flex flex-col justify-center gap-10 md:flex-row md:gap-7">
                 <FavoritePhoto />
                 <Baking />
+                <Foods />
             </div>
         </div>
     );
--- src/components/mainComponents/favorite/Baking.tsx
+++ src/components/mainComponents/favorite/Baking.tsx
@@ -1,20 +1,26 @@
 import { FC } from "react";
-
-import bushdenoel from "../../../assets/images/bushbenoel.jpg"
-import cheesecake from "../../../assets/images/cheesecake.jpg"
-import creampuff from "../../../assets/images/creampuff.jpg"
-import tart from "../../../assets/images/tart.jpg"
 import GridPhotos from "./GridPhotos";
+
+import bushdenoel from "../../../assets/images/bake/bushbenoel.jpg"
+import cake from "../../../assets/images/bake/cake.jpg"
+import cheesecake from "../../../assets/images/bake/cheesecake.jpg"
+import gateauchocolat from "../../../assets/images/bake/gateauchocolat.jpg"
+import snowballcookie from "../../../assets/images/bake/snowballcookie.jpg"
+import tart from "../../../assets/images/bake/tart.jpg"
 
 const Baking: FC = () => {
     const images = [
         (<img src={bushdenoel} alt="bushdenoel" className="w-full h-full object-cover" />),
+        (<img src={cake} alt="cake" className="w-full h-full object-cover" />),
+        (<img src={gateauchocolat} alt="gateushocolat" className="w-full h-full object-cover" />),
+        (<img src={snowballcookie} alt="snowbollcookie" className="w-full h-full object-cover" />),
         (<img src={cheesecake} alt="cheesecake" className="w-full h-full object-cover" />),
-        (<img src={creampuff} alt="creempuff" className="w-full h-full object-cover" />),
         (<img src={tart} alt="tart" className="w-full h-full object-cover" />)
     ];
 
     return (
+        <div className="flex flex-col items-center gap-5">
+        <h2 className="text-3xl font-bold font-zenKurenaido text-center md:text-5xl">Baking</h2>
         <div className="mockup-phone">
             <div className="camera" />
             <div className="display">
@@ -24,6 +30,7 @@ const Baking: FC = () => {
                     </div>
                 </div>
             </div>
+        </div>
         </div>
     );
 };
