1cf6d7974b7f7a43257682fcdab507a7a4c0547e
haruki26 <167726701+haruki26@users.noreply.github.com>
2024-08-07 00:28:42 +0900

    refactor: Improve Favorite component structure and imports

--- src/components/mainComponents/Favorite.tsx
+++ src/components/mainComponents/Favorite.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, MouseEvent, useRef, useState } from "react";
 import FavoritePhoto from "./favorite/FavoritePhoto";
 import Baking from "./favorite/Baking";
 import Foods from "./favorite/Foods";
@@ -10,10 +10,29 @@ const Favorite: FC = () => {
         { title: "Foods", main: <Foods /> }
     ]
 
+    const titleRef = useRef<HTMLDivElement>(null);
+    const contentRef = useRef<HTMLDivElement>(null);
+
     const [isVisivleContent, setIsVisibleContent] = useState<JSX.Element | null>(null)
 
     const handleContent = (index: number) => {
         setIsVisibleContent(contents[index].main)
+        
+        contentRef.current?.scrollIntoView({
+            behavior: "smooth",
+            block: "center",
+            inline: "center"
+        });
+    }
+
+    const handleBackBtn = () => {
+        titleRef.current?.scrollIntoView({
+            behavior: "smooth",
+            block: "center",
+            inline: "center"
+        });
+
+        setIsVisibleContent(null)
     }
 
     return (
@@ -22,8 +41,8 @@ const Favorite: FC = () => {
                 <div className="camera" />
                 <div className="display">
                     <div className="artboard artboard-demo phone-1">
-                        <div className={`w-full h-full flex gap-10 ${(isVisivleContent !== null) && "overflow-x-scroll"}`}>
-                            <div className="min-w-full m-auto flex flex-col gap-5">
+                        <div className={`w-full h-full flex gap-10 snap-mandatory snap-x ${(isVisivleContent !== null) ? "overflow-x-scroll" : "overflow-x-hidden"}`}>
+                            <div ref={titleRef} className="min-w-full m-auto flex flex-col gap-5 snap-center">
                                 {contents.map(({title}, index) => (
                                     <label
                                     key={index}
@@ -33,10 +52,10 @@ const Favorite: FC = () => {
                                     </label>
                                 ))}
                             </div>
-                            <div className="min-w-full flex flex-col px-3 py-5 overflow-y-scroll">
+                            <div ref={contentRef} className="min-w-full flex flex-col px-3 py-5 snap-center overflow-y-scroll">
                                 <label
                                 role="button"
-                                onClick={() => setIsVisibleContent(null)}
+                                onClick={handleBackBtn}
                                 className="w-full p-2 sticky top-0 underline text-3xl font-bold font-zenKurenaido">
                                     close
                                 </label>
