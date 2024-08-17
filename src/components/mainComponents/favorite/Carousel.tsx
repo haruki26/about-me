084d66cdf5438d03dc68618bdacac522a8025288
haruki26 <167726701+haruki26@users.noreply.github.com>
2024-08-18 02:31:34 +0900

    refactor: Update FavoritePhoto component with new images and layout

--- src/components/mainComponents/favorite/Carousel.tsx
+++ src/components/mainComponents/favorite/Carousel.tsx
@@ -1,18 +1,50 @@
-import { FC } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 
 type Props = {
-    images: JSX.Element[];
+    images: string[];
+    clickImage: number;
 }
 
-const Carousel: FC<Props> = ({images}) => {
+const Carousel: FC<Props> = ({images, clickImage}) => {
+    const clickImageRef = useRef<HTMLDivElement>(null);
+    const [visivleImage, setVisibleImage] = useState<number>(clickImage);
+
+    useEffect(() => {
+        clickImageRef.current?.scrollIntoView({
+            behavior: "instant",
+            block: "center",
+            inline: "center"
+        });
+    })
+
+    const handleArrowBtn = (index: number) => {
+        if (index < 0) {
+            setVisibleImage(images.length - 1);
+        } else if (index >= images.length) {
+            setVisibleImage(0);
+        } else {
+            setVisibleImage(index);
+        }
+    }
+
     return (
-        <div className="max-w-sm px-5">
-            <div className="carousel carousel-center rounded-box w-full space-x-4">
+        <div className="w-full h-full">
+            <div className="w-5/6 h-4/5 mx-auto flex gap-10 overflow-x-scroll hidden-scrollbar snap-x md:hidden">
                 {images.map((image, index) => (
-                    <div id={`item${index}`} key={index} className="carousel-item w-full h-96" >
-                        {image}
-                    </div>
+                    <div
+                    ref={(clickImage === index) ? clickImageRef : null}
+                    key={`photo-${index}`}
+                    className="min-w-full h-full snap-center">
+                        <img src={image} alt="photo" className="w-full h-full object-cover rounded-lg" />
+                    </div> 
                 ))}
+            </div>
+            <div className="rounded-box w-full hidden md:inline">
+                <div className="flex justify-between">
+                    <button onClick={() => handleArrowBtn(visivleImage - 1)} className="bg-gray-200 p-2 rounded-full">{"<"}</button>
+                    <img src={images[visivleImage]} alt="carousel" className="w-full" />
+                    <button onClick={() => handleArrowBtn(visivleImage + 1)} className="bg-gray-200 p-2 rounded-full">{">"}</button>
+                </div>
             </div>
         </div>
     );
--- src/components/mainComponents/favorite/FavoritePhoto.tsx
+++ src/components/mainComponents/favorite/FavoritePhoto.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { createRef, FC, RefObject, useMemo, useRef, useState } from "react";
 import { MdOutlineCameraAlt } from "react-icons/md";
 
 import kaigan from "../../../assets/images/kaigan.jpg";
@@ -7,6 +7,7 @@ import kibune from "../../../assets/images/kibune.jpg";
 import kibune2 from "../../../assets/images/kibune2.jpg";
 import kosumosu from "../../../assets/images/kosumosu.jpg";
 import higanbana from "../../../assets/images/higanbana.jpg";
+import Carousel from "./Carousel";
 
 
 const FavoritePhoto: FC = () => {
@@ -15,6 +16,13 @@ const FavoritePhoto: FC = () => {
     ];
 
     const [isVisivleModal, setIsVisibleModal] = useState<boolean>(false);
+    const [clickImage, setClickImage] = useState<number>(0);
+
+    const openModal = (index: number) => {
+        setClickImage(index);
+        setIsVisibleModal(true);
+    };
+
 
     return (
         <div className="w-full flex flex-col items-center gap-3">
@@ -24,12 +32,21 @@ const FavoritePhoto: FC = () => {
             </div>
             <div className="w-full max-w-screen-sm px-3 grid grid-cols-2 grid-flow-row gap-2 md:max-w-screen-md md:grid-cols-3">
                 {images.map((image, index) => (
-                    <div key={`photo-${index}`} className="w-full">
+                    <div key={`photo-${index}`} className="w-full" onClick={() => openModal(index)}>
                         <img src={image} alt="photo" className="aspect-square object-cover rounded-lg" />
                     </div> 
                 ))}
             </div>
-            {
+            {isVisivleModal && (
+                <div className="fixed top-0 left-0 w-screen h-screen bg-slate-700/70 z-50">
+                    <div className="w-full h-full flex flex-col">
+                        <div>
+                            <button onClick={() => setIsVisibleModal(false)} className="p-2 text-white">Close</button>
+                        </div>
+                        <Carousel images={images} clickImage={clickImage} />
+                    </div>
+                </div>
+            )
                 
             }
         </div>
