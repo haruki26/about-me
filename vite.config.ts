677e3b68dcb6689a578f9875dd611d026c91ea70
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2024-08-30 10:58:26 +0900

    refactor: Update lazy loading for create and home routes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/about-me",
+  define: {
+    __APP_ENV__: process.env.VITE_VERCEL_ENV
+  },
   plugins: [
     TanStackRouterVite(),
     viteReact(),
